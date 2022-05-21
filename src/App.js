ebb86440325592cf12ac436179ef0e1da6898b1e
dhrubobari <bari.dhrubo@gmail.com>
2022-05-22 04:26:06 +0600

    login form, sign up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Home/Shared/Navbar/Navbar';
+import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
+import SignUp from './Pages/Login/SignUp';
 
 function App() {
   return (
@@ -11,8 +14,14 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/appointment' element={<Appointment />}></Route>
+        <Route path='/appointment' element={
+        <RequireAuth>
+        <Appointment />
+        </RequireAuth>
+        }></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Appointments/Booking.js
+++ src/Pages/Home/Appointments/Booking.js
@@ -4,21 +4,21 @@ const Booking = ({ booking, setTreatment }) => {
     const {name, slots} = booking;
 
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-        <div class="card-body text-center">
-          <h2 class="text-lg font-bold text-secondary">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+        <div className="card-body text-center">
+          <h2 className="text-lg font-bold text-secondary">{name}</h2>
           <p>{
                 slots.length > 0
                 ? <span>{slots[0]}</span>
                 :<span className='text-red-500'>Try another day</span>
             }</p>
           <p className="">{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-          <div class="card-actions justify-center">
+          <div className="card-actions justify-center">
             <label
-            for="booking-modal" // booking-modal id from BookingModal.js to open
+            htmlFor="booking-modal" // booking-modal id from BookingModal.js to open
             disabled={slots.length === 0} 
             onClick={() => setTreatment(booking)}
-            class="btn btn-sm btn-secondary text-white uppercase">Book Appointment</label>
+            className="btn btn-sm btn-secondary text-white uppercase">Book Appointment</label>
           </div>
         </div>
       </div>
